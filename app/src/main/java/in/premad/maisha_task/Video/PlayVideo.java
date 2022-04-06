ce6e5e33db3ea15a136629587ff300f7497353ea
officialravindra <rsnarukaalwar@gmail.com>
2022-04-06 15:02:10 +0530

    play video

---
+++ app/src/main/java/in/premad/maisha_task/Video/PlayVideo.java
@@ -0,0 +1,147 @@
+package in.premad.maisha_task.Video;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.VideoView;
+
+import com.google.android.exoplayer2.ExoPlayerFactory;
+import com.google.android.exoplayer2.SimpleExoPlayer;
+import com.google.android.exoplayer2.extractor.DefaultExtractorsFactory;
+import com.google.android.exoplayer2.extractor.ExtractorsFactory;
+import com.google.android.exoplayer2.source.ExtractorMediaSource;
+import com.google.android.exoplayer2.source.MediaSource;
+import com.google.android.exoplayer2.trackselection.AdaptiveTrackSelection;
+import com.google.android.exoplayer2.trackselection.DefaultTrackSelector;
+import com.google.android.exoplayer2.trackselection.TrackSelector;
+import com.google.android.exoplayer2.ui.SimpleExoPlayerView;
+import com.google.android.exoplayer2.upstream.BandwidthMeter;
+import com.google.android.exoplayer2.upstream.DefaultBandwidthMeter;
+import com.google.android.exoplayer2.upstream.DefaultDataSourceFactory;
+import com.google.android.exoplayer2.upstream.DefaultHttpDataSourceFactory;
+
+import in.premad.maisha_task.R;
+
+public class PlayVideo extends AppCompatActivity {
+
+    int REQUEST_TAKE_GALLERY_VIDEO = 100;
+    // creating a variable for exoplayerview.
+    SimpleExoPlayerView exoPlayerView;
+    String filemanagerstring;
+    String selectedImagePath;
+    // creating a variable for exoplayer
+    SimpleExoPlayer exoPlayer;
+    String path;
+    Button pickup;
+
+    // url of video which we are loading.
+    String videoURL = "https://media.geeksforgeeks.org/wp-content/uploads/20201217163353/Screenrecorder-2020-12-17-16-32-03-350.mp4";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_play_video);
+        exoPlayerView = findViewById(R.id.idExoPlayerVIew);
+        pickup = findViewById(R.id.pickup);
+
+        /*Intent intent = getIntent();
+        path = intent.getStringExtra("path");
+        Log.d("TAG", "play screen" + path);*/
+
+
+        pickup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent1 = new Intent();
+                intent1.setType("video/*");
+                intent1.setAction(Intent.ACTION_GET_CONTENT);
+                startActivityForResult(Intent.createChooser(intent1, "Select Video"), REQUEST_TAKE_GALLERY_VIDEO);
+
+            }
+        });
+
+
+
+
+
+    }
+
+
+
+
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == RESULT_OK) {
+            if (requestCode == REQUEST_TAKE_GALLERY_VIDEO) {
+                Uri selectedImageUri = data.getData();
+                Log.d("uri",selectedImageUri.toString());
+
+                // OI FILE Manager
+                filemanagerstring = selectedImageUri.toString();
+
+                // MEDIA GALLERY
+               // selectedImagePath = getPath(selectedImageUri);
+      try {
+
+            // bandwisthmeter is used for
+            // getting default bandwidth
+            BandwidthMeter bandwidthMeter = new DefaultBandwidthMeter();
+
+            // track selector is used to navigate between
+            // video using a default seekbar.
+            TrackSelector trackSelector = new DefaultTrackSelector(new AdaptiveTrackSelection.Factory(bandwidthMeter));
+
+            // we are adding our track selector to exoplayer.
+            exoPlayer = ExoPlayerFactory.newSimpleInstance(this, trackSelector);
+
+
+            // we are parsing a video url
+            // and parsing its video uri.
+            Uri videouri = Uri.parse(filemanagerstring);
+
+
+
+            // we are creating a variable for datasource factory
+            // and setting its user agent as 'exoplayer_view'
+            DefaultHttpDataSourceFactory dataSourceFactory = new DefaultHttpDataSourceFactory("exoplayer_video");
+
+            // we are creating a variable for extractor factory
+            // and setting it to default extractor factory.
+            ExtractorsFactory extractorsFactory = new DefaultExtractorsFactory();
+
+            // we are creating a media source with above variables
+            // and passing our event handler as null,
+            //MediaSource mediaSource = new ExtractorMediaSource(selectedImageUri, dataSourceFactory, extractorsFactory, null, null);
+            MediaSource mediaSource = new  ExtractorMediaSource(videouri,
+                  new DefaultDataSourceFactory(PlayVideo.this,"exoplayer_video"),
+                  new DefaultExtractorsFactory(),null,null);
+
+
+
+            // inside our exoplayer view
+            // we are setting our player
+            exoPlayerView.setPlayer(exoPlayer);
+
+            // we are preparing our exoplayer
+            // with media source.
+            exoPlayer.prepare(mediaSource);
+
+            // we are setting our exoplayer
+            // when it is ready.
+            exoPlayer.setPlayWhenReady(true);
+
+        } catch (Exception e) {
+            // below line is used for
+            // handling our errors.
+            Log.e("TAG", "Error : " + e.toString());
+        }
+
+            }
+        }
+    }
+}
